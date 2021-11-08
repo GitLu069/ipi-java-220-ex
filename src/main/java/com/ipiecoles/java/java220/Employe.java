3da060b23c84e27013b9853f234fc135060402a4
Lucas Vincent <lucasvincent69250@gmail.com>
2021-11-08 17:23:05 +0100

    jusqu'à exo 107

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,5 +1,6 @@
 package com.ipiecoles.java.java220;
 import org.joda.time.LocalDate;
+import java.util.Objects;
 
 public class Employe {
 
@@ -11,6 +12,11 @@ public class Employe {
 
     //Constructeur
     public Employe() {
+        nom="nom";
+        prenom="prenom";
+        matricule="matricule";
+        dateEmbauche=LocalDate.now();
+        salaire=500d;
     }
 
     //Constructeur avec arguments
@@ -23,16 +29,29 @@ public class Employe {
         this.salaire = salaire;
     }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employe{");
+        sb.append("nom='").append(nom).append('\'');
+        sb.append(", prenom='").append(prenom).append('\'');
+        sb.append(", matricule='").append(matricule).append('\'');
+        sb.append(", dateEmbauche=").append(dateEmbauche);
+        sb.append(", salaire=").append(salaire);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, prenom, matricule, dateEmbauche, salaire);
+    }
+
     public final Integer getNombreAnneeAnciennete() {
         return LocalDate.now().getYear() - dateEmbauche.getYear();
     }
 
-
-    public String toString() {
-        String r = "Employe{nom=" + this.nom + ", prenom=" + this.prenom + ", matricule=" + this.matricule + ", dateEmbauche=" + this.dateEmbauche + ", salaire=" + this.salaire + "}";
-    }
-
     public Integer getNbConges(){
+
         return Entreprise.NB_CONGES_BASE;
     }
 
@@ -64,13 +83,9 @@ public class Employe {
         return dateEmbauche;
     }
 
-    public void setDateEmbauche(LocalDate NewDateEmbauche) {
-        if (NewDateEmbauche.isBefore(this.dateEmbauche)){
-            System.out.println("La date d'embauche ne peut être postérieure à la date courante");
-
-        }
-        else{
-            this.dateEmbauche=NewDateEmbauche;
+    public void setDateEmbauche(LocalDate dateEmbauche) throws Exception {
+        if(dateEmbauche != null && dateEmbauche.isAfter(LocalDate.now())) {
+            throw new Exception("La date d'embauche ne peut être postérieure à la date courante");
         }
         this.dateEmbauche = dateEmbauche;
     }
