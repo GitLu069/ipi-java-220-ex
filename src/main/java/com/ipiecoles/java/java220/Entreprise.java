b7c7daf0aad0ebc511f424508aa88d75ae46f142
Lucas Vincent <lucasvincent69250@gmail.com>
2021-12-08 19:20:20 +0100

    Ajout de la branch eval

--- src/main/java/com/ipiecoles/java/java220/Entreprise.java
+++ src/main/java/com/ipiecoles/java/java220/Entreprise.java
@@ -1,17 +1,21 @@
 package com.ipiecoles.java.java220;
-
 import org.joda.time.LocalDate;
 
-public final class Entreprise {
+public class Entreprise{
+
+    //Déclaration des variable
     public static final Double SALAIRE_BASE = 1480.27;
     public static final Integer NB_CONGES_BASE = 25;
-    public static final Integer NB_RTT_BASE = 12;
     public static final Double INDICE_MANAGER = 1.3;
-    public static final Double PRIME_MANAGER_PAR_TECHNICIEN = 250d;
-    public static final Double PRIME_ANCIENNETE = 100d;
+    public static final Double PRIME_MANAGER_PAR_TECHNICIEN = 250.0;
+    public static final Double PRIME_ANCIENNETE=100.0;
 
-    public static Double primeAnnuelleBase() {
-        return LocalDate.now().getYear() * 0.5;
+    //Constructor
+    public Entreprise(){
+
     }
-
+    //
+    public static Double primeAnnuelleBase(){
+        return LocalDate.now().getYear() /2d;
+    }
 }
